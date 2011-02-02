0c1607a659c58a4314826648c3e41b1866b240f3
kiy0taka <kiy0taka333+github@gmail.com>
2011-02-03 01:50:15 +0900

    change ordinal.

--- src/main/java/hudson/plugins/jquery_ui/JQueryUI.java
+++ src/main/java/hudson/plugins/jquery_ui/JQueryUI.java
@@ -3,7 +3,7 @@ package hudson.plugins.jquery_ui;
 import hudson.Extension;
 import hudson.model.PageDecorator;
 
-@Extension
+@Extension(ordinal=5)
 public class JQueryUI extends PageDecorator {
 
     public JQueryUI() {
